1fe6bf71dd5d39e75c7f29b8db358b83f27c87ae
Dmitry Teslya <d.a.teslya@gmail.com>
2021-06-03 03:35:17 +0400

    Updated versions of dependencies. Added integration sample.

--- cloud-feign/src/main/java/ru/samples/spring/cloud/feign/Application.java
+++ cloud-feign/src/main/java/ru/samples/spring/cloud/feign/Application.java
@@ -1,12 +1,12 @@
 package ru.samples.spring.cloud.feign;
 
-import feign.hystrix.FallbackFactory;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FallbackFactory;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
---
+++ integration/src/main/java/integration/Application.java
@@ -0,0 +1,106 @@
+package integration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.annotation.Gateway;
+import org.springframework.integration.annotation.MessagingGateway;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.core.MessagingTemplate;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.IntegrationFlows;
+import org.springframework.integration.kafka.dsl.Kafka;
+import org.springframework.integration.splitter.DefaultMessageSplitter;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.core.BeanFactoryMessageChannelDestinationResolver;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Slf4j
+@EnableKafka
+@EnableScheduling
+@EnableIntegration
+@SpringBootApplication
+public class Application {
+
+    private int index = 0;
+    private static final int COUNT = 3;
+
+    @Autowired
+    private MessagingTemplate messagingTemplate;
+    @Autowired
+    private RecordQueue recordQueue;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Scheduled(initialDelay = 5_000, fixedDelay = 15_000)
+    public void putMessage() {
+        String message = IntStream.range(index, index + COUNT)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(","));
+        log.info("Put: {}", message);
+        recordQueue.put(message);
+        index += COUNT;
+    }
+
+    @MessagingGateway
+    interface RecordQueue {
+        @Gateway(requestChannel = "inputFlow.input")
+        void put(Object payload);
+    }
+
+    @Bean
+    public IntegrationFlow inputFlow(KafkaTemplate<?, ?> kafkaTemplate) {
+        DefaultMessageSplitter splitter = new DefaultMessageSplitter();
+        splitter.setDelimiters(",");
+        return f -> f
+                .log("split")
+                .split(splitter)
+                .log("message")
+                .handle((MessageHandler) kafkaTemplate::send);
+    }
+
+    @Bean
+    public IntegrationFlow kafkaFlow(
+            ConsumerFactory<?, ?> consumerFactory,
+            ProducerFactory<?, ?> producerFactory) {
+        return IntegrationFlows
+                .from(Kafka.messageDrivenChannelAdapter(consumerFactory, "kafka.input"))
+                .log("kafka")
+                .handle(Kafka.outboundChannelAdapter(producerFactory).topic("kafka.output"))
+                .get();
+    }
+
+    @Bean
+    public MessagingTemplate messagingTemplate() {
+        MessagingTemplate messagingTemplate = new MessagingTemplate();
+        messagingTemplate.setDestinationResolver(new BeanFactoryMessageChannelDestinationResolver());
+        return messagingTemplate;
+    }
+
+    @KafkaListener(topics = "kafka.output")
+    public void listenKafkaOutput(Message<?> message) {
+        messagingTemplate.send("outputFlow.input", message);
+    }
+
+    @Bean
+    public IntegrationFlow outputFlow() {
+        return f -> f
+                .log("aggregate")
+                .aggregate()
+                .log("release");
+    }
+}
